bc6f53aaea5e68a6ea9fe73623c34dddd3063e09
Aaron Ang <awz.ang@gmail.com>
2017-02-03 23:35:47 -0800

    Update grid for techs

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -9,11 +9,10 @@ export function Tech(props) {
   return (
     <div className="row">
       <div className="col-3 hidden-sm"></div>
-      <div className="col-6">
+      <div className="col-9">
         <p className="title"><a href={props.url}>{props.title}</a></p>
         <p className="tech-info">{new Date(props.pubDate).toLocaleDateString()} by <a href="#" className="publisher">{capitalizeFirstLetter(props.publisher)}</a></p>
       </div>
-      <div className="col-3 hidden-sm"></div>
     </div>
   )
 }
