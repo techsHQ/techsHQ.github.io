e2896dcaf25f381babd71d3ce6f6d8eb1fc6e314
Aaron Ang <awz.ang@gmail.com>
2017-02-03 23:22:46 -0800

    Display techs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
+import './Reset.css';
 import './App.css';
 import logo from './logo.svg';
+import { Tech } from './components/Tech'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      techs: [],
+      next: '',
+      prev: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://dry-ocean-29667.herokuapp.com/api/techs')
+      .then(result => result.json())
+      .then(json => {
+        console.log(json.data)
+        this.setState({
+          techs: json.data.techs,
+          next: json.data.next,
+          prev: json.data.prev
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         <header>
           <div className="container">
             <div className="row">
-              <div className="col-1 hidden-sm"></div>
-            	<div className="col-10">
+              <div className="col-3 hidden-sm"></div>
+            	<div className="col-6">
                 <a href="/"><img className="logo" src={logo} alt="Techs logo."/></a>
             	</div>
-              <div className="col-1 hidden-sm"></div>
+              <div className="col-3 hidden-sm"></div>
             </div>
           </div>
         </header>
         <main>
+          <div className="container">
+            {this.state.techs.map((tech, index) =>
+              <Tech url={tech.url} publisher={tech.publisher} title={tech.title} pubDate={tech.pub_date} key={index} />
+            )}
 
+          </div>
         </main>
       </div>
     );
---
+++ src/components/Tech.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Tech.css'
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function Tech(props) {
+  return (
+    <div className="row">
+      <div className="col-3 hidden-sm"></div>
+      <div className="col-6">
+        <p className="title"><a href={props.url}>{props.title}</a></p>
+        <p className="tech-info">{new Date(props.pubDate).toLocaleDateString()} by <a href="#" className="publisher">{capitalizeFirstLetter(props.publisher)}</a></p>
+      </div>
+      <div className="col-3 hidden-sm"></div>
+    </div>
+  )
+}
