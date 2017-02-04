f657a4472053c3b4aa5744a1b6db929017bef3f9
Aaron Ang <awz.ang@gmail.com>
2017-02-03 21:51:11 -0800

    Add Simple Grid and header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header>
+          <div className="container">
+            <div className="row">
+              <div className="col-1 hidden-sm"></div>
+            	<div className="col-10">
+                <a href="/"><img className="logo" src={logo} alt="Techs logo."/></a>
+            	</div>
+              <div className="col-1 hidden-sm"></div>
+            </div>
+          </div>
+        </header>
+        <main>
+
+        </main>
       </div>
     );
   }
