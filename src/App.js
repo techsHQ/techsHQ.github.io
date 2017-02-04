dfaf55b28aae02a86eb94f653875268f2e6df3ec
Aaron Ang <awz.ang@gmail.com>
2017-02-04 00:08:42 -0800

    Add navigation without browser history support

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import './Reset.css';
 import './App.css';
 import logo from './logo.svg';
 import { Tech } from './components/Tech'
+import { Navigation } from './components/Navigation'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      techs: [],
-      next: '',
-      prev: ''
-    }
+  state = {
+    techs: [],
+    next: '',
+    prev: ''
   }
 
   componentDidMount() {
     fetch('https://dry-ocean-29667.herokuapp.com/api/techs')
+      .then(result => result.json())
+      .then(json => {
+        console.log(json.data)
+        this.setState({
+          techs: json.data.techs,
+          next: json.data.next,
+          prev: json.data.prev
+        })
+      })
+  }
+
+  handleNavigation = event => {
+    event.preventDefault()
+    var href = event.target.getAttribute('href')
+    fetch(href)
       .then(result => result.json())
       .then(json => {
         console.log(json.data)
@@ -47,7 +59,7 @@ class App extends Component {
             {this.state.techs.map((tech, index) =>
               <Tech url={tech.url} publisher={tech.publisher} title={tech.title} pubDate={tech.pub_date} key={index} />
             )}
-
+            <Navigation handleNavigation={this.handleNavigation} prev={this.state.prev} next={this.state.next} />
           </div>
         </main>
       </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Navigation(props) {
+
+  return (
+    <div className="row">
+      <div className="col-3 hidden-sm"></div>
+      <div className="col-9">
+        <nav>
+          {props.prev ? (<a href={props.prev} onClick={props.handleNavigation}>previous</a>) : ('')}
+          {props.prev ? ' | ' : ''}
+          <a href={props.next} onClick={props.handleNavigation}>next</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
