a43545af690be751b2c2f7aca5e9d19c04e086ad
Aaron Ang <awz.ang@gmail.com>
2017-02-04 09:51:06 -0800

    Open articles in new tab

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -9,7 +9,8 @@ export function Tech(props) {
   return (
     <div className="row">
       <div className="col-12">
-        <p className="title"><a href={props.url}>{props.title}</a></p>
+        <p className="title"><a href={props.url} target="_blank">{props.title} <i className="fa fa-external-link fa-1" aria-hidden="true"></i>
+</a></p>
         <p className="tech-info">{new Date(props.pubDate).toLocaleDateString()} by <a href="#" className="publisher">{capitalizeFirstLetter(props.publisher)}</a></p>
       </div>
     </div>
