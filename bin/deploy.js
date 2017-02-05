93f1aad624c360c5fadd936ea6df1e69928c9158
Aaron Ang <awz.ang@gmail.com>
2017-02-05 10:45:33 -0800

    Simplify deploy

---
+++ bin/deploy.js
@@ -0,0 +1,15 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+ghpages.publish(path.join(__dirname, '../build'), {
+  branch: 'master',
+  repo: 'git@github.com:techsHQ/techsHQ.github.io.git',
+  message: 'Update website',
+  add: true
+}, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Deployed successfully.')
+  }
+});
